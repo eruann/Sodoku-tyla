543e021aa55782cb3384eaae71f6b7a94565af39
Matu <Matias.M.Rodriguez@gmail.com>
2019-06-23 14:31:21 -0300

    Comentarios al codigo

--- C#/sudoku cs/Game.cs
+++ C#/sudoku cs/Game.cs
@@ -15,23 +15,30 @@ namespace sudoku_cs
         public delegate void ShowSolutionEventHandler(int[][] grid);
 
         /* Declaro 3 array de tipo lista de Enteros de tamaño 9 , la "lista" de enteros permite metodos
-        adicionales a los de los array basicos, en especial me interesa el metodo "intersect"
+        adicionales a los de los array basicos, en especial me interesa el metodo "intersect" que viene (del namespace linq
+        el cual permite agregarle a c# la funcionalidad de hacer "queries" orientadas al lengaje de programacion)
+        trae un subset de elementos que estan en ambos arrays, por lo que permite chequear si un elemento esta repetido
         */
         /*
+        Declaraciones necesarias para usar el generador de sudokus
         private List<int>[] HRow = new List<int>[9];
         private List<int>[] VRow = new List<int>[9];
         private List<int>[] ThreeSquare = new List<int>[9];
         */
+
         /* Declaro un jagged array de  de tipo Entero de tamaño 9, un jagged array es un "array de arrays"
         el primer bracket indica el tamaño del jagged array, 9 en este caso indicando que guarda hasta 9 "arrays" 
         el segundo bracket indica la dimensionalidad, en este caso son array de 1 sola dimension, si fueran de mas 
         se indicarian con comas    private int[2][ , ] guardiara 2 array de 2 dimensiones cada uno
-        
+        En este caso el Jagged array contendra cada fila del sudoku y cada elemento de los array de fila seran una celda
              */
         private int[][] grid = new int[9][];
  
-        //Declaro  generador de numeros aleatorios https://docs.microsoft.com/en-us/dotnet/api/system.random?view=netframework-4.8 
-        // 
+        /*
+        Declaro  generador de numeros aleatorios https://docs.microsoft.com/en-us/dotnet/api/system.random?view=netframework-4.8 
+        originalmente se usa para generar numeros para el armado de sudokus, pero se cambia su proposito para seleccionar
+        aleatoriamente uno de los sudokus predefenidos.
+        */
         private Random r;
         public void NewGame(Random rn)
         {
@@ -39,17 +46,22 @@ namespace sudoku_cs
             createNewGame();
         }
 
-        //Inicializo las  listas y el array creado anteriormente
+        /*Inicializo las  listas y el array creado anteriormente.Este paso se mantiene  por buena practica, 7
+        ya que no siendo necesaria la generacion automatica, se podria inicializar y cargar datos en un mismo paso
+        */
         private void initializeLists()
         {
             
             for (int x = 0; x <= 8; x++)
             {
+                //Inicilizo los array previamente creados, 
                 /*
                 HRow[x] = new List<int>(new int[] { 1, 2, 3, 4, 5, 6, 7, 8, 9 });
                 VRow[x] = new List<int>(new int[] { 1, 2, 3, 4, 5, 6, 7, 8, 9 });
                 ThreeSquare[x] = new List<int>(new int[] { 1, 2, 3, 4, 5, 6, 7, 8, 9 });
                 */
+                //inicializo el jagged array grid, poniendo en cada posicion un array de 9 posiciones vacio
+
                 int[] row = new int[9];
                 grid[x] = row;
             }
@@ -59,7 +71,7 @@ namespace sudoku_cs
 
         private void createNewGame()
         {
-            
+            //reutilizo el random declardo previamente para generar un numero entre 0 y 9 que seleccionara el sudoku a usar
             int Randomsudoku = r.Next(0, 9);
             switch (Randomsudoku)
             {
@@ -175,7 +187,18 @@ namespace sudoku_cs
                     break;
             }
 
-            
+            /*
+            En resumidas cuentas el generador de sudokus trabaja por fuerza bruta  
+             "int si = (y / 3) * 3 + (x / 3);" ubica la posicion dentro de cada uno de los elementos de los cuadrados
+             de 3x3 , mientras que y recorre las filas y x las columnas. Inicialmente array "grid" se encuentra lleno
+             con los numeros del 1 al 9 de la inicilizacion, por lo que la formula va recorriendo e intersectando cada 
+             elemento de los 3 arrays (Filas, columnas, cuadrados 3x3) y removiendo los repetidos. 
+             luego reemplaza el numero por otro aleatorio  y vuelve a comparar todos los elementos hasta que cada casilla
+             esta completa por un numero unico y "util" es decir de acuerdo a las reglas del sudoku
+             esta estrategia si bien es utilizable utiliza sentencias "goto" para resolver el problema, por una cuestion
+             de buena programacion en general se desaconseja el uso de este tipo de sentencias. por lo cual se 
+             reemplaza con 10 sudokus estaticos.
+             */
 /*
             while (true)
             {
